d16b84d0e2f222080d288ae62b8f0fc68aa418c2
samjoshuva <sam613263@gmail.com>
2019-05-04 16:06:54 +0530

    migration file

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
-const Migrations = artifacts.require("Migrations");
+const Bid = artifacts.require("Bid");
 
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+module.exports = function (deployer) {
+  deployer.deploy(Bid);
 };
