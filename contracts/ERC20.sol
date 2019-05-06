47dab03541b1a490b2022d0331427db0c4b96b35
samjoshuva <sam613263@gmail.com>
2019-05-06 22:59:21 +0530

    vsting added

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -1,3 +1,4 @@
+
 pragma solidity ^0.5.0;
 
 contract ERCToken {
@@ -9,12 +10,16 @@ contract ERCToken {
     address public owner;
 
     mapping(address => uint) balances;
+    
+    uint public currentTime;
+    
+    
 
     constructor() public payable {
         symbol = "SJ";
         name = "DUMMY Token";
         decimals = 10;
-        _totalSupply = 100000000000000000000000000;
+        _totalSupply = 100;
         owner = msg.sender;
         balances[owner] = _totalSupply;
     }
@@ -24,17 +29,24 @@ contract ERCToken {
     }
 
 
-    function transfer(address to, uint tokens) public payable  returns (bool success) {
+    function transfer(address to, uint tokens) public payable vesting  returns (bool success) {
         balances[msg.sender] = balances[msg.sender] - tokens;
         balances[to] = balances[to] + tokens;
         // emit Transfer(msg.sender, to, tokens);
+        currentTime = block.timestamp;
         return true;
     }
     
-    function transferFrom(address from, address to, uint tokens ) public payable returns(bool success){
+    function transferFrom(address from, address to, uint tokens ) public payable vesting returns(bool success){
         balances[from] = balances[from] - tokens;
         balances[to] = balances[to] + tokens;
         return true;
+    }
+    
+    modifier vesting(){
+        require(block.timestamp >=currentTime + 5,"please wait for 5 sec to Transfer next token");
+
+        _;
     }
     
 }
